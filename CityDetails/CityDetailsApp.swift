e4b2a17f4287feaede629cbe27c1c092fe1051e5
Akash-Gupta-yash <ag.office00@gmail.com>
2024-10-28 22:19:01 +0530

    Initial Commit

---
+++ CityDetails/CityDetailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityDetailsApp.swift
+//  CityDetails
+//
+//  Created by Deeksha Verma on 28/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CityDetailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
